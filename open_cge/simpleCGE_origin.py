706dc3a5c4ee6aecce5e1d4c78342e048b47e2b2
quyennguyen2201 <13261867+quyennguyen2201@users.noreply.github.com>
2023-10-04 13:25:53 +1300

    Supress warnig

--- open_cge/simpleCGE_origin.py
+++ open_cge/simpleCGE_origin.py
@@ -1,7 +1,3 @@
-# add directory 
-import sys
-sys.path.append('../')
-
 # import packages
 import numpy as np
 from pandas import Series, DataFrame
@@ -9,10 +5,9 @@ from open_cge import government as gov
 from open_cge import household as hh
 from open_cge import aggregates as agg
 from open_cge import firms
-import warnings
+import warnings 
 
-
-def cge_system(pvec, cge_args):
+def cge_system(pvec, args):
     '''
     This function solves the system of equations that represents the
     CGE model.
@@ -24,14 +19,14 @@ def cge_system(pvec, cge_args):
     Returns:
         p_error (Numpy array): Errors from CGE equations
     '''
-    (p, data, ind, h, Z, Q, Kd, pd, Ff, R, er) = cge_args
+    (p, d, ind, h, Z, Q, Kd, pd, Ff, R, er) = args
 
     py = pvec[0:len(ind)]
     pf = pvec[len(ind): len(ind) + len(h)]
     py = Series(py, index=list(ind))
     pf = Series(pf, index=list(h))
 
-    pm = firms.eqpm(er, data.import_price_index)
+    pm = firms.eqpm(er, d.pWm)
     pq = firms.eqpq(pm, pd, p.taum, p.eta, p.deltam, p.deltad, p.gamma)
     Kk = agg.eqKk(pf, Ff, R, p.lam, pq)
     Kf = agg.eqKf(Kk, Kd)
@@ -43,10 +38,10 @@ def cge_system(pvec, cge_args):
     Sp = agg.eqSp(p.ssp, pf, Ff, Fsh, Trf)
     I = hh.eqI(pf, Ff, Sp, Td, Fsh, Trf)
 
-    pf_error = agg.eqpf(F, data.factor_endowment_0)
-    pk_error = agg.eqpk(F, Kk, data.capital_0, data.factor_endowment_0)
+    pf_error = agg.eqpf(F, d.Ff0)
+    pk_error = agg.eqpk(F, Kk, d.Kk0, d.Ff0)
     py_error = firms.eqpy(p.b, F, p.beta, Y)
-    
+
     # Suppress the FutureWarning
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=FutureWarning)
@@ -62,4 +57,3 @@ def cge_system(pvec, cge_args):
     p_error = np.append(py_error, pf_error)
 
     return p_error
-
